16d742102691b1378445de989e1612a7cb83c53d
Ryan Janvier <ryan@liquidtrends.com>
2014-10-27 21:38:07 -0700

    Add jQuery functionality to employee and tasks. Add draggable employee function using jQuery ui

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,40 @@
 $(function(){
 	
-	$("input").on("keyup", function(e) {
+	// Show Employee Input //
 
-		console.log(this);
+	$("#add-employee").click(function() {
+		$( "#employeeInput" ).show();
+		$( "#employee-list" ).sortable();
+		$( "#task-column" ).droppable(); 
+	});
+
+	// Employee Input //
+	$("#employeeInput").on("keyup", function(e) {
+
 		if (e.keyCode == 13) {
 		var $this = $(this);
 
 		var newItem = ($this.val());
 		if (newItem !== "") {
-			$("ul").append($("<li>").text(newItem).addClass("resource-item"));
+			$("ul#employee-list").append($("<li>").text(newItem).addClass("employee-box"));
 			$this.val('');
 			$("button#random").fadeIn(700);
+		  }
+		}
+
+	});
+
+	// Add new tasks //
+	$("#taskInput").on("keyup", function(e) {
+
+		if (e.keyCode == 13) {
+		var $this = $(this);
+
+		var newItem = ($this.val());
+		if (newItem !== "") {
+			$("div#task-list1").append($("<h2>").text(newItem).addClass("task-item"));
+			$this.val('');
+			$(".task-input").fadeOut(700);
 		  }
 		}
 
